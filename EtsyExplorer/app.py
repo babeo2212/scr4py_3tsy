fba04831f82947da9e6379cf72094d46a54a2a22
hungvt <vutienhung2212@gmail.com>
2023-08-29 17:57:56 +0700

    Add context manager to file app.py line 38, add one more save file as shopname.csv for better data archive.

--- EtsyExplorer/app.py
+++ EtsyExplorer/app.py
@@ -35,9 +35,9 @@ def run():
   # Make image folder
   mkdir()
   # Starting scrawling session
-  process = CrawlerProcess(settings=get_project_settings())
-  process.crawl(EtsyspiderSpider, shopname=shopname, numpage = numpage)
-  process.start()
+  with CrawlerProcess(settings=get_project_settings()) as process:
+    process.crawl(EtsyspiderSpider, shopname=shopname, numpage = numpage)
+    process.start()
   # Convert csv file into excel
   fileConverter(filename=shopname)
   # userCustomName = "shopABC"
