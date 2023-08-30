94f6f681de09602777000f0341f205baacee173a
hungvt <vutienhung2212@gmail.com>
2023-08-30 16:47:13 +0700

    Finish app in win_ver

---
+++ EtsyExplorer/EtsyExplorer/sendEmail.py
@@ -0,0 +1,44 @@
+# from scrapy.mail import MailSender 
+import os
+import logging
+import smtplib
+from datetime import datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+def sendEmail(recipient=None, message=None):
+  # mailer = MailSender()
+  userEmail = os.environ["SCRAPY_USEREMAIL"]
+  userPass = os.environ["SCRAPY_USERPASS"]
+  
+  if not recipient:
+    recipient = os.environ["SCRAPY_DEFAULT_RECIPIENT"]
+
+  msg = MIMEMultipart()
+  msg["From"] = userEmail
+  msg["To"] = recipient
+  msg["Subject"] = "Scrapy Report"
+  if not message:
+    message = f"""
+    Da lay du lieu xong luc {datetime.now().strftime("%H:%m %d-%m-%Y")}.
+  """
+  
+  msg.attach(MIMEText(message))
+  img = MIMEImage(open("meme.jpg", "rb").read())
+  img.add_header("Content-Disposition", "attachment; filename='image.jpg'")
+  msg.attach(img)
+  logger.info("Sending Email.")
+  with smtplib.SMTP("smtp.gmail.com", 587) as mailServer:
+    mailServer.ehlo()
+    mailServer.starttls()
+    mailServer.login(userEmail, userPass)
+    mailServer.sendmail(userEmail, recipient, msg.as_string())
+  logger.info("Email sent.")
+
+sendEmail("nguyenthuy221210@gmail.com")
+# print(os.getcwd())
--- EtsyExplorer/app.py
+++ EtsyExplorer/app.py
@@ -2,6 +2,7 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from EtsyExplorer.spiders.etsySpider import EtsyspiderSpider
 from EtsyExplorer.helperFunc import fileConverter, imgDownloaderConcurrency, mkdir
+from EtsyExplorer.sendEmail import sendEmail
 import logging
 
 logging.basicConfig(
@@ -13,40 +14,49 @@ logger = logging.getLogger(__name__)
 # logger.addHandler(handler)
 
 def run():
+  ###########################################################################################
   # Get shop name
-  # shopname = input("Please insert SHOP NAME to crawl: ")
-  # if not shopname:
-  #   return
-  # while True:
-  #   try:
-  #     # Get num page to crawl
-  #     numpage = input("Please insert number of page to crawl: ")
-  #     if not numpage or numpage == '0':
-  #       numpage = 1_000_000
-  #     else:
-  #       numpage = int(numpage)
-  #   except ValueError:
-  #     print("Not a valid number.")
-  #   else:
-  #     break
-  # userCustomName = input("Please insert your FILE NAME: ")
-  # if not userCustomName:
-  #   userCustomName = shopname
-  # # Make image folder
-  # mkdir()
-  # # Starting scrawling session
-  # process = CrawlerProcess(settings=get_project_settings())
-  # # process = CrawlerProcess(settings=get_project_settings())
-  # process.crawl(EtsyspiderSpider, shopname=shopname, numpage = numpage)
-  # process.start()
+  shopname = input("Please insert SHOP NAME to crawl: ")
+  if not shopname:
+    return
+  while True:
+    try:
+      # Get num page to crawl
+      numpage = input("Please insert number of page to crawl: ")
+      if not numpage or numpage == '0':
+        numpage = 1_000_000
+      else:
+        numpage = int(numpage)
+    except ValueError:
+      print("Not a valid number.")
+    else:
+      break
+  userCustomName = input("Please insert your FILE NAME: ")
+  recipient = input("Please insert recipient name: ")
+  if not userCustomName:
+    userCustomName = shopname
+  if not recipient:
+    recipient = None
+  ###########################################################################################
+  # Make image folder
+  mkdir()
+  ###########################################################################################
+  # Starting scrawling session
+  process = CrawlerProcess(settings=get_project_settings())
+  process.crawl(EtsyspiderSpider, shopname=shopname, numpage = numpage)
+  process.start()
+  ###########################################################################################
   # Convert csv file into excel
-  shopname = "BaileyDesignedCo"
   fileConverter(filename=shopname)
-  # userCustomName = "shopABC"
-  # imgDownloaderConcurrency(userCustomName)
+  ###########################################################################################
+  sendEmail(recipient)
+  ###########################################################################################
+  ans = input("Scrawling completed. Do you want to download all images? [y|n]").lower()
+  if ans == "y":
+    # userCustomName = "shopABC"
+    imgDownloaderConcurrency(userCustomName)
+  else:
+    return
 
 if __name__ == "__main__":
-  # BaileyDesignedCo
-  # MayaPrintDesign
-  # OzzieDigitalArt
   run()
